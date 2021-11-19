421309aee174693701cc36d2d12816d6739fba9e
Francesco-Ostellari <francescoostellari@gmail.com>
2021-11-19 17:02:18 +0100

    end-bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,10 +1,5 @@
-/* Descrizione
-Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+/* 
+  il programma deve controllare se l'utente ha inserito qualcosa nel prompt o meno
 */
 
 // chiedo all'utente il numero di km che vuole percorrere
@@ -13,25 +8,34 @@ console.log("numero di km da percorrere:", chilometri);
 let eta = parseInt(prompt ("Inserisci la tua età:"));
 console.log("età persona:", eta);
 
-// calcolo prezzo base biglietto
-const costoAlKm = 0.21;
-let costoBase = (costoAlKm * chilometri);
-console.log ("Costo base biglietto:", costoBase);
+if (!(isNaN(chilometri)) && !(isNaN(eta)) && chilometri !='' && eta != '') {
 
-// stampo a video il costo finale del biglietto
-document.getElementById('costoBase').innerHTML = 'Il prezzo del biglietto è di: ' + parseFloat(costoBase).toFixed(2) + '€.';
+  // calcolo prezzo base biglietto
+  const costoAlKm = 0.21;
+  let costoBase = (costoAlKm * chilometri);
+  console.log ("Costo base biglietto:", costoBase);
 
-// Prezzo base per età compresa tra i 18 e 65 anni
-let prezzoScontato = costoBase;
+  // stampo a video il costo finale del biglietto
+  document.getElementById('costoBase').innerHTML = 'Il prezzo del biglietto è di: ' + parseFloat(costoBase).toFixed(2) + '€.';
 
-// calcolo prezzo biglietto con sconti
-if (eta < 18) {
-  prezzoScontato = (costoBase - ((costoBase * 20) / 100));
-} else if (eta > 65) {
-  prezzoScontato = (costoBase - ((costoBase * 40) / 100));
+  // Prezzo base per età compresa tra i 18 e 65 anni
+  let prezzoScontato = costoBase;
+
+  // calcolo prezzo biglietto con sconti
+  if (eta < 18) {
+    prezzoScontato = (costoBase - ((costoBase * 20) / 100));
+  } else if (eta > 65) {
+    prezzoScontato = (costoBase - ((costoBase * 40) / 100));
+  }
+  console.log("Il prezzo scontato del biglietto è di:", prezzoScontato);
+
+  // stampo a video il costo finale del biglietto
+  document.getElementById('prezzoScontato').innerHTML = 'Il prezzo scontato del biglietto è di: ' + parseFloat(prezzoScontato).toFixed(2) + '€.';
+
+} else if (chilometri == '' || isNaN(chilometri)) {
+  console.log("Valore per i chilometri non corretto, inserire nuovamentee il valore");
+  document.writeln("Valore per i chilometri non corretto, inserire nuovamentee il valore");
+} else if (eta == '' || isNaN(eta)) {
+  console.log("Valore per l'età non corretto, inserire nuovamente il valore");
+  document.writeln("Valore per l'età non corretto, inserire nuovamente il valore");
 }
-console.log("Il prezzo scontato del biglietto è di:", prezzoScontato);
-
-// stampo a video il costo finale del biglietto
-document.getElementById('prezzoScontato').innerHTML = 'Il prezzo scontato del biglietto è di: ' + parseFloat(prezzoScontato).toFixed(2) + '€.';
-
